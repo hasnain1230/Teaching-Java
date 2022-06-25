53c16bb7ea3017b3155ae63168b3f7ab3fcb8413
hasnain1230 <hasnain1230@gmail.com>
2022-06-25 11:16:35 -0400

    Added Data Types and basic string files.

---
+++ Data_Types.java
@@ -0,0 +1,13 @@
+public class Data_Types {
+	public static void main(String[] args) {
+		double num1 = 52.5;
+		double num2 = 22.22222222;
+
+		System.out.println(num1 + num2);
+
+		int myNum1 = 5;
+		int myNum2 = -2;
+
+		System.out.println(myNum1 - myNum2);
+	}
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+
+		// //System.out.println("Hello there Joey" + args);
+	}	
+}
+/*
+Array: Defined by a Data Type. In this case, args is a String array. The args array can hold multiple different strings passed in through the command line at run time. 
+args = {input0, input1, input2, ... }
+
+java Main testing testing1
+
+After running the above, args[0] will store "testing" and args[1] will store "testing1"
+
+   */
